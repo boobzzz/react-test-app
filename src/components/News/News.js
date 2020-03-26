bd18bbde54ec9a1a3da21f79f9c32b8ef3b00d6f
Yuriy Borys <iam618384@gmail.com>
2020-03-27 00:27:54 +0200

    Add redux to the project

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -1,28 +1,29 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-// import * as A from '../../utilities/api.js';
+
+import * as A from '../../utils/api.js';
 
 import Article from './Article/Article'
 
-// const API_SOURCE = 'http://newsapi.org/v2/everything?q=bitcoin&from=2020-02-23&sortBy=publishedAt&apiKey=65dfd0d404834ea98af2ffe8f1dd8db5'
-
 const News = (props) => {
-    // const [articles, setArticles] = useState([]);
-    console.log(props.articles);
-
     useEffect(() => {
-        return props.onComponentMount
-    })
+        const fetchData = async () => {
+            const API_SOURCE = 'http://newsapi.org/v2/everything?domains=wsj.com,nytimes.com&apiKey=65dfd0d404834ea98af2ffe8f1dd8db5'
+            const data = await A.fetchGET(API_SOURCE)
+            return props.onComponentMount(data.articles)
+        }
+        fetchData()
+    }, [])
 
+    console.log(props.articles)
     return (
         <section>
-            {/* {props.articles.map((article, i) =>
+            {props.articles.map((article, i) =>
                 <Article
                     key={i}
                     name={article.title}
                     author={article.author}
-                    desc={article.description} />)} */}
-            NEWS PAGE
+                    desc={article.description} />)}
         </section>
     )
 }
@@ -35,7 +36,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        onComponentMount: () => dispatch({type: 'FETCH'})
+        onComponentMount: (arr) => dispatch({type: 'FETCH', fetched: arr})
     }
 }
 
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,20 +1,10 @@
-import * as A from '../utils/api.js';
-
-const API_SOURCE = 'http://newsapi.org/v2/everything?domains=wsj.com,nytimes.com&apiKey=65dfd0d404834ea98af2ffe8f1dd8db5'
-
 const initialState = {
     articles: [],
 }
 
-export const articlesReducer = async (state = initialState, action) => {
-    const data = await A.fetchGET(API_SOURCE)
-    console.log(data.articles);
-
+export const articlesReducer = (state = initialState, action) => {
     if (action.type === 'FETCH') {
-        return {
-            ...state,
-            articles: [...state.articles, data.articles]
-        }
+        return {...state, articles: [...state.articles, ...action.fetched]}
     }
-    console.log(state.articles);
+    return state
 }
